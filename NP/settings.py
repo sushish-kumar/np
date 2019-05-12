5f5d8e19db53d50b352ef01bb388dfec96360f32
sushishk <kumarsushish51@gmail.com>
2019-05-12 19:41:36 +0530

    update static root

--- NP/settings.py
+++ NP/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
 
